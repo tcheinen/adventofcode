cfd1e90da79563112317c82ef25a30fa6c7220ae
Teddy Heinen <skyshayde@gmail.com>
2018-12-01 10:43:29 -0600

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+dupe = -1
+frequencies = [0]
+with open("input.txt") as f:
+    for line in f:
+        freq = frequencies[-1] + int(line)
+        frequencies.append(freq)
+    print("ANSWER 1: " + str(frequencies[-1]))
+    while(dupe == -1):
+        f.seek(0)
+        for line in f:
+            freq = frequencies[-1] + int(line)
+            frequencies.append(freq)
+        set_ = set()
+        for item in frequencies:
+            if item in set_:
+                dupe = item
+                break
+            set_.add(item)
+    print("ANSWER 2: " + str(dupe))
+
