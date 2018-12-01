b4ee3f53dab868b585fe3adb219a019684325b1b
Teddy Heinen <skyshayde@gmail.com>
2018-12-01 14:11:45 -0600

    easy efficiency boost by batching

--- day1/day1.py
+++ day1/day1.py
@@ -6,10 +6,11 @@ with open("input.txt") as f:
         frequencies.append(freq)
     print("ANSWER 1: " + str(frequencies[-1]))
     while(dupe == -1):
-        f.seek(0)
-        for line in f:
-            freq = frequencies[-1] + int(line)
-            frequencies.append(freq)
+        for i in range(10):
+            f.seek(0)
+            for line in f:
+                freq = frequencies[-1] + int(line)
+                frequencies.append(freq)
         set_ = set()
         for item in frequencies:
             if item in set_:
