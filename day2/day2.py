5b50a72039d4b689363dc2c7fc8f1eb443d5f362
Teddy Heinen <skyshayde@gmail.com>
2018-12-02 00:35:40 -0600

    clean up a few things for day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,30 +1,22 @@
-count2 = 0
-count3 = 0
 import collections
 import itertools
-import difflib
+
 with open("input.txt") as f:
+    count2 = 0
+    count3 = 0
     for line in f:
-        d = collections.Counter(line)
-        c2 = False
-        c3 = False
-        for i in d.values():
-            if(i == 2):
-                c2 = True
-            if(i == 3):
-                c3 = True
-        if(c2):
+        d = list(collections.Counter(line).values())
+        if(2 in d):
             count2 += 1
-        if(c3):
+        if(3 in d):
             count3 += 1
-print("Answer 1: " + str(count2 * count3))
-checksums = []
-with open("input.txt") as f:
-    for line in f:
-        checksums.append(line.rstrip())
-length = len(checksums[0])
-for a, b in itertools.combinations(checksums,2):
-    l = [i for i,(a1,a2)  in enumerate(zip(a,b)) if a1!=a2]
-    if(len(l) == 1):
-        print("Answer 2: " + a[:l[0]] + a[l[0]+1:])
+    print("Answer 1: " + str(count2 * count3))
+
+    f.seek(0)
+    checksums = [x.rstrip() for x in f.readlines()]
+    for a, b in itertools.combinations(checksums,2):
+        l = [i for i,(a1,a2)  in enumerate(zip(a,b)) if a1!=a2]
+        if(len(l) == 1):
+            print("Answer 2: " + a[:l[0]] + a[l[0]+1:])
+            break
 
