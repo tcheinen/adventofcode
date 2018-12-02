4990f60a62c84b4cedec75ee64e7aa110721efb7
Teddy Heinen <skyshayde@gmail.com>
2018-12-01 20:37:40 -0600

    make part 2 run more efficiently

--- day1/day1.py
+++ day1/day1.py
@@ -1,21 +1,19 @@
-dupe = -1
-frequencies = [0]
+import itertools
+
+base = [0]
 with open("input.txt") as f:
     for line in f:
-        freq = frequencies[-1] + int(line)
-        frequencies.append(freq)
-    print("ANSWER 1: " + str(frequencies[-1]))
-    while(dupe == -1):
-        for i in range(10):
-            f.seek(0)
-            for line in f:
-                freq = frequencies[-1] + int(line)
-                frequencies.append(freq)
-        set_ = set()
-        for item in frequencies:
-            if item in set_:
-                dupe = item
-                break
-            set_.add(item)
-    print("ANSWER 2: " + str(dupe))
+        freq = base[-1] + int(line)
+        base.append(freq)
+base.pop(0)
+mod = [divmod(i,base[-1]) for i in base]
+mod_indexes = []
+for a, b in itertools.combinations(enumerate(mod),2):
+    if(a[1][1] != b[1][1]):
+        continue
+    mod_indexes.append(((a[0],b[0]), abs(a[1][0] - b[1][0])))
+mod_indexes.sort(key=lambda tup: tup[1])
+freq = mod_indexes[0]
 
+print("Answer 1: " + str(base[-1]))
+print("Answer 2: " + str(freq[1] * base[-1] + base[freq[0][0]]))
