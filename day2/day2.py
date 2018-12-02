dfafadc5aeee30d8f4dd04438efa51635cad7ca3
Teddy Heinen <skyshayde@gmail.com>
2018-12-02 14:29:12 -0600

    only read file line by line once

--- day2/day2.py
+++ day2/day2.py
@@ -4,16 +4,15 @@ import itertools
 with open("input.txt") as f:
     count2 = 0
     count3 = 0
-    for line in f:
+    checksums = [x.rstrip() for x in f.readlines()]
+    for line in checksums:
         d = list(collections.Counter(line).values())
         if(2 in d):
             count2 += 1
         if(3 in d):
             count3 += 1
     print("Answer 1: " + str(count2 * count3))
-
-    f.seek(0)
-    checksums = [x.rstrip() for x in f.readlines()]
+    
     for a, b in itertools.combinations(checksums,2):
         l = [i for i,(a1,a2)  in enumerate(zip(a,b)) if a1!=a2]
         if(len(l) == 1):
