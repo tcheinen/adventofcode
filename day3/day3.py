e1fce1f4b4c0db42b9e4aadc42929eacb340c9fd
Teddy Heinen <skyshayde@gmail.com>
2018-12-03 00:25:32 -0600

    only read from file once

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 grid = defaultdict(int)
+answer_1 = 0
 answer_2 = -1
 def parse(data):
     x,y = [int(x) for x in line[line.find("@ ")+2:line.find(": ")].split(",")]
@@ -9,26 +10,26 @@ def parse(data):
     return (id, x, y, w, h)
 
 with open("input.txt") as f:
+    suits = []
     for line in f:
-        id, x, y, w, h = parse(line)
+        suits.append(parse(line))
+    for suit in suits:
+        id, x, y, w, h = suit    
         for i in range(w):
             for j in range(h):
                 grid[(x + i, y + j)] += 1
-    f.seek(0)
-    for line in f:
-        id, x, y, w, h = parse(line)
+    for suit in suits:
+        id, x, y, w, h = suit    
         overlapping = False
         for i in range(w):
             for j in range(h):
-                if(grid[(x + i, y + j)] != 1):
+                if(grid[(x + i, y + j)] > 1):
                     overlapping = True
         if(overlapping == False):
             answer_2 = id
 
-
-overlap_counter = 0
 for i in grid.values():
     if(i > 1):
-        overlap_counter += 1
-print("Answer 1: " + str(overlap_counter))
+        answer_1 += 1
+print("Answer 1: " + str(answer_1))
 print("Answer 2: " + str(answer_2))
